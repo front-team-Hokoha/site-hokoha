78dceb356680de91c86e35d3606272d070c8fe68
ylorgueilleux <ylorgueilleux@hokoha.com>
2024-09-18 09:06:48 +0200

    Démarrage du site Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    
+// Options Astro et Vite par défaut
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, // option pour garantir le Hot-Reload
+      },
+    },
+  },
+});
