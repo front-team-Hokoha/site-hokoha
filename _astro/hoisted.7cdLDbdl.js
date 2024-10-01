34bdb48c5bc241bc4fcc8dc143d1cd4ffde5595a
front-team-Hokoha <front-team-Hokoha@users.noreply.github.com>
2024-10-01 07:12:42 +0000

    deploy: dfd02647ea611b53a9e4d18325e5330cd6233a58

--- _astro/hoisted.7cdLDbdl.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.x6TKZHTO.js";window.addEventListener("load",function(){const e=document.getElementById("video_pattern");e.style.backgroundImage="url('/site-hokoha/assets/img/trame-video.jpg')"});
