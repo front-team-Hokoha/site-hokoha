2e0cee0f16f1984c099148efbd3bc40515d28527
Ewen BETIN <e.betin@hokoha.com>
2024-10-29 09:34:53 +0100

    swiper => nombre de slides selon taille de l'écran

--- src/libs/swiper.js
+++ src/libs/swiper.js
@@ -6,13 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const swiper = new Swiper('.swiper', {
         modules: [Autoplay],
         spaceBetween: 24,
-        slidesPerView: 10,
+        slidesPerView: 4, // Nombre de slides par défaut à afficher lorsqu'aucune règle le précise (breakpoints)
         loop: true,
         speed: 4000,
-        // allowTouchMove: false, --> empecher l'utilisateur de scroll avec son pointeur de souris
         autoplay: {
             delay: 0,
             disableOnInteraction: false, 
+        },
+        breakpoints: {
+            // Lorsque la largeur de la fenêtre est >= 768px
+            768: {
+                slidesPerView: 5,
+            },
+            // Lorsque la largeur de la fenêtre est >= 1280px
+            1280: {
+                slidesPerView: 10,
+            },
         },
     });
 });
