826b5d5bc0253404ada5b7c7336e7bd122bba544
Ewen BETIN <e.betin@hokoha.com>
2024-10-29 14:29:41 +0100

    swiper =>  + barre de défilement ne fonctionne uniquement lorsqu'elle se trouve dans le viewport  + les images ne se chargent seulement lorsqu'on arrive a la barre de défilement et non pas lorsque l'on arrive sur la page.

--- src/libs/swiper.js
+++ src/libs/swiper.js
@@ -2,26 +2,58 @@ import Swiper from 'swiper';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const swiper = new Swiper('.swiper', {
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperContainer = document.querySelector('.swiper');
+    const logoContainers = document.querySelectorAll('.logo-container');
+
+    // Initialiser le Swiper
+    const swiper = new Swiper(swiperContainer, {
         modules: [Autoplay],
         spaceBetween: 24,
-        slidesPerView: 4, // Nombre de slides par défaut à afficher lorsqu'aucune règle le précise (breakpoints)
+        slidesPerView: 4,
         loop: true,
         speed: 4000,
+        grabCursor: true,
         autoplay: {
             delay: 0,
-            disableOnInteraction: false, 
+            disableOnInteraction: false,
+            enabled: false,
         },
         breakpoints: {
-            // Lorsque la largeur de la fenêtre est >= 768px
-            768: {
-                slidesPerView: 5,
-            },
-            // Lorsque la largeur de la fenêtre est >= 1280px
-            1280: {
-                slidesPerView: 10,
-            },
+            768: { slidesPerView: 5 },
+            1280: { slidesPerView: 10 },
         },
     });
+
+    // Fonction pour charger l'image
+    const loadImage = (container) => {
+        const logoPath = container.getAttribute('data-logo-path');
+        const altText = container.getAttribute('data-alt-text');
+        const img = new Image();
+        img.src = logoPath; // charger l'image
+        img.alt = altText;
+        container.appendChild(img); // ajouter l'image au conteneur
+    };
+
+    // Observer pour charger les images lorsque le conteneur est visible
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                loadImage(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    // Observer chaque conteneur de logo
+    logoContainers.forEach(container => observer.observe(container));
+
+    // Observer le conteneur swiper pour gérer l'autoplay
+    const swiperObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.isIntersecting ? swiper.autoplay.start() : swiper.autoplay.stop();
+        });
+    });
+
+    swiperObserver.observe(swiperContainer);
 });
