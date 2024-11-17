7da4954c0c50aa026dd899536663393b68db62a3
ylorgueilleux <ylorgueilleux@hokoha.com>
2024-11-17 12:30:05 +0100

    recherche design colors

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,5 @@
 // Defines a debounce function to limit the rate at which a function can fire.
-export const debounce = (func, delay) => {
+const debounce = (func, delay) => {
     let timerId; // Holds a reference to the timeout between calls.
     return (...args) => {
       clearTimeout(timerId); // Clears the current timeout, if any, to reset the debounce timer.
@@ -8,3 +8,28 @@ export const debounce = (func, delay) => {
       }, delay);
     };
   };
+
+  // Returns the mouse position
+const getMousePos = e => {
+  return { 
+      x : e.clientX, 
+      y : e.clientY 
+  };
+};
+
+// Returns the window width and height
+const getWinSize = () => {
+  return { 
+      width: window.innerWidth, 
+      height: window.innerHeight 
+  };
+};
+
+const isFirefox = () => navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+export {
+  debounce,
+  getMousePos,
+  getWinSize,
+  isFirefox,
+};
