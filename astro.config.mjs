5baa9ce238c4d37d3e6c2e3b5bcabb03d3f74304
Yann Lorgueilleux <y.lorgueilleux@hokoha.com>
2026-04-15 05:29:58 +0200

    deploiment auto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'astro/config'; // Déprécié
 // https://astro.build/config
 export default defineConfig({
    site: 'https://www.hokoha.com', 
+
+   base: '/repo-name/',  // Le nom du dépôt GitHub
+       output: 'static',     // Production en mode site statique
+
    devToolbar: {
       enabled: false
    }
